30c2eeb8176828090f631b19c1ae2bd2f0a25c31
matan <matanlavi10@gmail.com>
2020-05-20 00:47:03 +0300

    fix(): fix some notes

--- Vue/Exercise1/src/App.vue
+++ Vue/Exercise1/src/App.vue
@@ -2,34 +2,19 @@
   <v-app>
     <v-content>
       <v-container class="light-blue lighten-5" fluid fill-height>
-        <v-row style="height: 100%">
+        <v-row class="fullHeight">
           <v-col class="col-3" pl-5>
-            <SideMenu @changeTitle="changeTitle" style="height: 100%" />
+            <SideMenu @changeTitle="changeTitle" class="fullHeight" />
           </v-col>
           <v-col class="col-9" fill-height pr-5>
-            <v-flex style="height: 66%">
+            <v-flex v-bind:class="[showExtraDetails ? 'twoThirdsHeight' : 'fullHeight']">
               <Details :currentTitle="currentTitle" />
             </v-flex>
-            <v-flex pt-3 style="height: 34%" v-if="this.showExtraDetails">
-              <ExtraDetails style="height: 100%" />
+            <v-flex class="thirdHeight" pt-3 v-if="showExtraDetails">
+              <ExtraDetails class="fullHeight" />
             </v-flex>
           </v-col>
         </v-row>
-        <!-- <v-layout row>
-          <v-flex pl-5 md3 xs3>
-            <SideMenu @changeTitle="changeTitle($event)" style="height: 100%" />
-          </v-flex>
-          <v-flex pr-5 md9 xs9>
-            <v-layout column style="height: 100%">
-              <v-flex style="height: 66%">
-                <Details :currentTitle="this.currentTitle" />
-              </v-flex>
-              <v-flex pt-3 style="height: 34%" v-if="this.showExtraDetails">
-                <ExtraDetails style="height: 100%" />
-              </v-flex>
-            </v-layout>
-          </v-flex>
-        </v-layout> -->
       </v-container>
     </v-content>
   </v-app>
@@ -65,3 +50,16 @@ export default {
 };
 </script>
 
+<style scoped>
+.fullHeight {
+  height: 100%;
+}
+
+.twoThirdsHeight {
+  height: 66%;
+}
+
+.thirdHeight {
+  height: 33%;
+}
+</style>
